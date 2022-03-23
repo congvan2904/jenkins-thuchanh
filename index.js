187b6f8b6594dfbb829637be3cbed85313f72266
congvan2904 <nguyenvancong.nvc@gmail.com>
2022-03-23 07:54:25 +0700

    them express cors

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const PORT = 8080
+
+app.use(cors())
+app.get('/', (req, res) => {
+    res.send({
+        message: 'thanh cong', status: 200, data:
+            ['Reactnative',
+                'ReactJs',
+                'HTML',
+                'Css',
+                'Sass']
+
+    })
+})
+
+
+app.listen(PORT, () => console.log(`Dang chay ${PORT}`))
